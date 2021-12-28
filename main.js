58e49c34b1119d35f5ffdec76a69c34d35770e5a
Gboi123 <85195842+Gboi123@users.noreply.github.com>
2021-12-28 12:11:38 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,57 @@
 function startClassification(){
     navigator.mediaDevices.getUserMedia({audio: true});
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/MJ31-vnhx/model.json', modelReady);
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/QeTTloPCR/model.json', modelReady);
 }
 
 function modelReady(){
     classifier.classify(gotResults);
 }
+function gotResults(error, results){
+    if (error) {
+        console.error(error);
+    } else{
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+    
+        document.getElementById("result_label").innerHTML = 'I can hear - '+
+        results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - '+
+        (results[0].confidence*100).toFixed(2)+"%";
+        document.getElementById("result_label").style.color = "rgb("
+        +random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color = "rgb("
+        +random_number_r+","+random_number_g+","+random_number_b+")";
+
+        img = document.getElementById('Dog');
+        img1 = document.getElementById('Chicken');
+        img2 = document.getElementById('Cat');
+        img3 = document.getElementById('Cow');
+
+        if (results[0].label == "Barking") {
+            img.src = 'dog.gif';
+            img1.src = 'chicken.png';
+            img2.src = 'cat.jpg';
+            img3.src = 'cow.png';
+        }
+        else if (results[0].label == "Chicken") {
+            img.src = 'dog.png';
+            img1.src = 'chicken.gif';
+            img2.src = 'cat.jpg';
+            img3.src = 'cow.png';
+        }
+        else if (results[0].label == "Meowing") {
+            img.src = 'dog.png';
+            img1.src = 'chicken.png';
+            img2.src = 'cat.gif';
+            img3.src = 'cow.png';
+        }
+        else{
+            img.src = 'dog.gif';
+            img1.src = 'chicken.png';
+            img2.src = 'cat.jpg';
+            img3.src = 'cow.gif';
+        }
+        }
+    }
